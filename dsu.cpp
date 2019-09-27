073ead1c26f5baca2972ec20287077279fdf34ff
kzvd4729 <kzvd4729@gmail.com>
2019-09-27 18:37:45 +0600

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,1 +1,22 @@
-
+/*
+Arpa's trick. Range min,max in O(1). Offline. Linear memory.
+Sort range query by their right side, answer every query ending i when 1 to i has
+been processed.
+*/
+int par[N+2],aa[N+2];
+inline int get(int x)
+{
+  if(x==par[x])return x;
+  return par[x]=get(par[x]);
+}
+void process(int n)
+{
+  stack<int>st;
+  for(int i=1;i<=n;i++)
+  {
+    while(st.size()&&aa[st.top()]<=aa[i])
+      par[st.top()]=i,st.pop();
+    par[i]=i;st.push(i);
+    /*answer all query ending at i*/
+  }
+}
