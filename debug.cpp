4b9b9dfc590010698e35730c1475ed8fd3582b1a
kzvd4729 <kzvd4729@gmail.com>
2019-10-07 17:11:16 +0600

    Create debug.cpp

---
+++ debug.cpp
@@ -0,0 +1,11 @@
+/****************************************/
+#define _sp(x) cerr<<#x<<" = "<<x<<endl
+#define _dp(x) cerr<<#x<<" = "<<x.first<<" "<<x.second<<endl
+#define _f cerr<<"flag"<<endl
+template<typename T>//container
+void _p(T v)
+{
+  cerr<<"( ";
+  for(auto x:v)cerr<<x<<" ";
+  cerr<<")"<<endl;
+}
