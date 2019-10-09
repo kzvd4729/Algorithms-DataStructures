f30c7cb6bd2bab9ece7ff177845e15d42c36ddf5
kzvd4729 <kzvd4729@gmail.com>
2019-10-09 12:10:04 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define long long long
+using namespace std;
+const int N=1e6;
+
+#define SP(x) cerr<<#x<<" = "<<x<<endl
+#define DP(x) cerr<<#x<<" = "<<x.first<<" "<<x.second<<endl
+#define F cerr<<"flag"<<endl
+template<typename T>//container
+void _p(T v)
+{
+  cerr<<"( ";
+  for(auto x:v)cerr<<x<<" ";
+  cerr<<")"<<endl;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);cin.tie(0);
+
+  return 0;
+}
