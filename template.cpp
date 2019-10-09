d7f95e6f5faa2dc558a64967987c40c7c3b1f33b
kzvd4729 <kzvd4729@gmail.com>
2019-10-09 15:40:38 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -11,6 +11,19 @@ void P(T v)
   for(auto x:v)cerr<<x<<" ";
   cerr<<")"<<endl;
 }
+void PB(int x)//print in binary format
+{
+  cerr<<x<<" = ";vector<int>v;
+  while(x){v.push_back(x%2);x/=2;}
+  for(int i=v.size()-1;i>=0;i--)cerr<<v[i];
+  cerr<<endl;
+}
+template<typename T>
+void UN(vector<T>&v)//make unique
+{
+  sort(v.begin(),v.end());
+  v.erase(unique(v.begin(),v.end()),v.end());
+}
 
 const int N=1e6;
 int main()
@@ -18,6 +31,7 @@ int main()
   //freopen("inp.txt","r",stdin);
   //freopen("out.txt","w",stdout);
   ios_base::sync_with_stdio(0);cin.tie(0);
+
 
   return 0;
 }
