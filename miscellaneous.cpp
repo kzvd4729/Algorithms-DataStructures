5285b3f815cbd1bd05f2f666b0325fc416389d52
kzvd4729 <kzvd4729@gmail.com>
2019-10-20 20:22:12 +0600

    Update miscellaneous.cpp

--- miscellaneous.cpp
+++ miscellaneous.cpp
@@ -5,3 +5,9 @@ for(int msk=0;msk<(1<<17);msk++)
   for(int sub=msk;sub>0;sub=(sub-1)&msk)
     cnt++;
 }
+//merge two sorted vector
+vector<int>a,b,c;
+a={1,2,3,4};
+b={2,5,6,7};
+c.resize((int)a.size()+(int)b.size());
+merge(a.begin(),a.end(),b.begin(),b.end(),c.begin());
