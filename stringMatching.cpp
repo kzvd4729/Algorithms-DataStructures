0ad074c009e3d4bd930d7e24a2d630ba8e211da3
kzvd4729 <kzvd4729@gmail.com>
2019-11-01 00:53:16 +0600

    Update stringMatching.cpp

--- stringMatching.cpp
+++ stringMatching.cpp
@@ -4,7 +4,7 @@ void doKmp(string s)
   int n=s.size();kmp[0]=0;
   for(int i=1;i<n;i++)
   {
-    int now=kmp[i-1];
+    int now=kmp[i-1],kmp[i]=0;
     while(true)
     {
       if(s[now]==s[i]){kmp[i]=now+1;break;}
