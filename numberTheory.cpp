c29f38d360d2203ae439eb60cf56d3e64725e4a5
kzvd4729 <kzvd4729@gmail.com>
2020-01-22 16:04:05 +0600

    Update numberTheory.cpp

--- numberTheory.cpp
+++ numberTheory.cpp
@@ -15,3 +15,16 @@ void calPhi(void)
     for(int j=i+i;j<=N;j+=i)phi[j]-=phi[i];
   }
 }
+
+/*
+res[n] = sumOf(i/gcd(i,n)) for i=1 to n
+*/
+long phi[N+2],res[N+2];
+void calPhi(void)
+{
+  for(int i=1;i<=N;i++)
+  {phi[i]+=i;for(int j=i+i;j<=N;j+=i)phi[j]-=phi[i];}
+  for(int i=1;i<=N;i++)
+  {for(int j=i;j<=N;j+=i)res[j]+=i*phi[i];}
+  for(int i=1;i<=N;i++)res[i]=(res[i]+1)/2;
+}
