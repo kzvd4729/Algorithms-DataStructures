909bd10b32ef7d4120820283bcb98f3a44303867
kzvd4729 <kzvd4729@gmail.com>
2020-03-02 00:11:19 +0600

    Update stirling.cpp

--- stirling.cpp
+++ stirling.cpp
@@ -11,17 +11,16 @@ formula:  ---  ∑   (-1)^i kCi (k-i)^n
 both k can be crossed out. after reformulating:
 (-1)^i 1/(i!)  *  1/(k-i)! (k-i)^n
 for fixed n we can calculte for all k using fft
-for stirling(3,2)={1 23} {2 13} {3 12}
+for stirling(3,2) there are three ways {1|23} {2|13} {3|12}
 */
 int fac[N+2];
 void init(int n)
 {
-  fac[0]=1;
-  for(int i=1;i<=n;i++)fac[i]=(1LL*fac[i-1]*i)%mod;
+  fac[0]=1;for(int i=1;i<=n;i++)fac[i]=(1LL*fac[i-1]*i)%mod;
 }
 void stirling(int n,int k)
 {
-  vector<int>a,b,c;init(N);
+  init(N);vector<int>a,b,c;
   for(int i=0;i<=n;i++)
   {
     a.push_back((big(-1,i)*big(fac[i],mod-2)+mod)%mod);
