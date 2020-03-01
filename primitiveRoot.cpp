4fe8b57144392f4780f369b4502aa7cb582ebf8f
kzvd4729 <kzvd4729@gmail.com>
2020-03-01 17:32:30 +0600

    Update primitiveRoot.cpp

--- primitiveRoot.cpp
+++ primitiveRoot.cpp
@@ -1,24 +1,31 @@
 /*
-we will only define here primitive root for prime number.
+we will only define here primitive root for prime number(modulo prime).
 a number g is called primitive root of prime p if g^i (1 to p-1) generates
 all number from 1 to p-1(in any order).
 primitive root is also called generator.
 */
-//a naive approach
-int primitiveRoot(int p)
+int big(int b,int p,int m)
 {
-  vector<int>v(p);v[0]=1;
-  for(int i=2;i<p;i++)
+  int ret=1;
+  while(p){if(p&1)ret=(1LL*ret*b)%m;b=(1LL*b*b)%m;p/=2;}
+  return ret;
+}
+int generator(int p)
+{
+  vector<int>fact;
+  int phi=p-1,n=phi;
+  for(int i=2;i*i<=n;i++)
   {
-    for(int j=1;j<p;j++)v[j]=0;
-    int pw=1,f=0;
-    for(int j=1;j<p;j++)
-    {
-      pw=(1LL*pw*i)%p;
-      if(v[pw]){f=1;break;}
-      v[pw]=1;
-    }
-    if(!f)return i;
+  	if(n%i)continue;fact.push_back(i);
+    while(n%i==0)n/=i;
+  }
+  if(n>1)fact.push_back(n);
+  for(int res=2;res<=p;res++)
+  {
+    bool ok=true;
+    for(size_t i=0;i<fact.size()&&ok;i++)
+      ok&=big(res,phi/fact[i],p)!=1;
+    if(ok)return res;
   }
   return -1;
 }
