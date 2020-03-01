ebc20e9f5cd0ce351ec71526b11ef7ce8b7af271
kzvd4729 <kzvd4729@gmail.com>
2020-03-01 17:11:56 +0600

    Update combinatorics.cpp

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -9,3 +9,26 @@ void calNCR(int n)
       ncr[i][j]=ncr[i-1][j-1]+ncr[i-1][j];////use mod
   }
 }
+
+const int mod=1e9+7;
+int big(int b,int p,int m=mod)
+{
+  int ret=1;
+  while(p){if(p&1)ret=(1LL*ret*b)%m;b=(1LL*b*b)%m;p/=2;}
+  return ret;
+}
+int fac[N+2];
+void init(int n)
+{
+  fac[0]=1;
+  for(int i=1;i<=n;i++)fac[i]=(1LL*fac[i-1]*i)%mod;
+}
+int ncr(int n,int r)
+{
+  int a=fac[n];a=(1LL*a*big(fac[r],mod-2))%mod;
+  return (1LL*a*big(fac[n-r],mod-2))%mod;
+}
+int npr(int n,int r)
+{
+  return (1LL*fac[n]*big(fac[n-r],mod-2))%mod;
+}
