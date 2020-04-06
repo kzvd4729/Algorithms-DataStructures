e14b140fe0d3007e0de3604a58e7f1825176e719
kzvd4729 <kzvd4729@gmail.com>
2020-04-06 12:57:48 +0600

    Create manacher.cpp

---
+++ manacher.cpp
@@ -0,0 +1,27 @@
+//od[i] is largest palindromic substring centered at i;
+vector<int>od;
+//ev[i] is largest palindromic substring centered at i and i+1;
+vector<int>ev;
+void manacher(string s)
+{
+  int n=s.size();
+  od.resize(n);
+  for(int i=0,l=0,r=-1;i<n;i++)
+  {
+    int k=(i>r)?1:min(od[l+r-i],r-i+1);
+    while(0<=i-k&&i+k<n&&s[i-k]==s[i+k])k++;
+    od[i]=k--;
+    if(i+k>r)l=i-k,r=i+k;
+  }
+  ev.resize(n);
+  for(int i=0,l=0,r=-1;i<n;i++)
+  {
+    int k=(i>r)?0:min(ev[l+r-i+1],r-i+1);
+    while(0<=i-k-1&&i+k<n&&s[i-k-1]==s[i+k])k++;
+    ev[i]=k--;
+    if(i+k>r)l=i-k-1,r=i+k;
+  }
+  for(int i=0;i<n;i++)od[i]=od[i]*2-1;
+  ev.erase(ev.begin());
+  for(int i=0;i<n-1;i++)ev[i]=ev[i]*2;
+}
