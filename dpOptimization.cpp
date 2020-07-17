321b1d3e9261e338805f4a4759e1f345934ea482
Araf Rahman <kzvd4729@gmail.com>
2020-07-17 20:31:11 +0600

    Update dpOptimization.cpp

--- dpOptimization.cpp
+++ dpOptimization.cpp
@@ -19,3 +19,92 @@ void dfs(int i,int lo,int hi,int lt,int rt)
   }
   dfs(i,lo,j-1,lt,op);dfs(i,j+1,hi,op,rt);
 }
+
+/*
+convexHull trick
+make sure before query set is non empty
+*/
+//line container: fast but doesn't support everywhere
+#pragma once
+struct line
+{
+  mutable long k,m,p;
+  bool operator<(const line& o)const{return k<o.k;}
+  bool operator<(long x)const{return p<x;}
+};
+struct lineContainer:multiset<line,less<>>
+{
+  //(for doubles,use inf=1/.0,div(a,b)=a/b)
+  const long inf=LLONG_MAX;
+  long div(long a,long b){return a/b-((a^b)<0&&a%b);}
+  bool isect(iterator x,iterator y)
+  {
+    if(y==end()){x->p=inf;return false;}
+    if(x->k==y->k)x->p=x->m>y->m?inf:-inf;
+    else x->p=div(y->m-x->m,x->k-y->k);
+    return x->p>=y->p;
+  }
+  void add(long k,long m)
+  {
+    //k*=-1;m*=-1;//for min
+    auto z=insert({k,m,0}),y=z++,x=y;
+    while(isect(y,z))z=erase(z);
+    if(x!=begin()&&isect(--x,y))isect(x,y=erase(y));
+    while((y=x)!=begin()&&(--x)->p>=y->p)
+      isect(x,erase(y));
+  }
+  long get(long x)
+  {
+    assert(!empty());
+    auto l=*lower_bound(x);
+    return (l.k*x+l.m);//for min return -;
+  }
+};
+
+//hull Dynamic: slow
+const long inv=-(1LL<<62);//doesn't matter min or max query
+bool Min;//true for min query false for max query
+struct line
+{
+  long m,b;
+  mutable function<const line*()>succ;
+  bool operator<(const line &rhs)const
+  {
+    if(rhs.b!=inv)return m<rhs.m;
+    const line *s=succ();
+    if(!s)return 0;
+    long x=rhs.m;
+    return b-s->b<(s->m-m)*x;
+  }
+};
+struct hullDynamic:public multiset<line>
+{
+  bool bad(iterator y)
+  {
+    auto z=next(y);
+    if(y==begin())
+    {
+      if(z==end())return 0;
+      return y->m==z->m&&y->b<=z->b;
+    }
+    auto x=prev(y);
+    if(z==end())return y->m==x->m&&y->b<=x->b;
+    return 1.0*(x->b-y->b)*(z->m-y->m)>=1.0*(y->b-z->b)*(y->m-x->m);
+  }
+  void add(long m,long b)
+  {
+    if(Min)m*=-1,b*=-1;
+    auto y=insert({m,b});
+    y->succ=[=]{return next(y)==end()?0:&*next(y);};
+    if(bad(y)){erase(y);return;}
+    while(next(y)!=end()&&bad(next(y)))erase(next(y));
+    while(y!=begin()&&bad(prev(y)))erase(prev(y));
+  }
+  long get(long x)
+  {
+    auto l=*lower_bound((line){x,inv});
+    long ret=l.m*x+l.b;
+    if(Min)ret*=-1;return ret;
+  }
+  void _clear(){clear();}
+}st;
