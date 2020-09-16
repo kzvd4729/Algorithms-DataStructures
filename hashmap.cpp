49fb6f3fd83d15f64f86732cb6c03dfb9ef0df5b
Araf Rahman <kzvd4729@gmail.com>
2020-09-16 11:37:11 +0600

    Create hashmap.cpp

---
+++ hashmap.cpp
@@ -0,0 +1,22 @@
+/*
+not been enough tested
+*/
+const int sz=1e7;//more size better
+int key[sz+2],val[sz+2];
+static inline int h(int x){return((x^179)*7LL)%sz;}
+void init()
+{
+  memset(key,-1,sizeof key);//-1 is invalid
+}
+void add(int x,int y)
+{
+  int k=h(x);
+  while(key[k]!=-1&&key[k]!=x)k=(k+1)%sz;//first non-taken index
+  key[k]=x,val[k]=y;
+}
+int get(int x)
+{
+  for(int k=h(x);key[k]!=-1;k=(k+1)%sz)
+    if(key[k]==x)return val[k];
+  return -1;
+}
