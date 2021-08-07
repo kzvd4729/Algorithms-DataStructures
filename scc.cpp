cd07993536346e2ea5ead5f863a420408fef5ef2
Araf Rahman <kzvd4729@gmail.com>
2021-08-08 00:06:38 +0600

    Update scc.cpp

--- scc.cpp
+++ scc.cpp
@@ -1,6 +1,6 @@
 /*
 strongly connected component for directed graph.
-adj vector stores the directed edges where rv stores reverse edges.
+ad vector stores the directed edges where rv stores reverse edges.
 consider a dag 1-->2-->3-->4-->5, topological order will be 5 4 3 2 1.
 
 consider a node u and an adjacent vertex of u in the reverse graph is v.
@@ -8,11 +8,11 @@ v should be in the right side side of u in topological order. if not there
 must be a cycle including u and v which messes up the topological order.
 */
 int vis[N+2];
-vector<int>adj[N+2],rv[N+2],ord,com;
+vector<int>ad[N+2],rv[N+2],ord,com;
 void topo(int node)
 {
   vis[node]=1;
-  for(auto x:adj[node])
+  for(auto x:ad[node])
     if(!vis[x])topo(x);
   ord.push_back(node);
 }
@@ -31,5 +31,14 @@ void solve(int n)
   {
     if(vis[ord[i]])continue;
     com.clear();dfs(ord[i]);
+
+    // work with the component
+
   }
+}
+void addEdge(int u,int v)// there is a directed edge from u to v
+{
+  // if(u==v)assert(0);
+  ad[u].push_back(v);
+  rv[v].push_back(u);
 }
