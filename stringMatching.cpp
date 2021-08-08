ced3271159b7b5aa49b57abcb5cc4a462091b616
Araf Rahman <kzvd4729@gmail.com>
2021-08-08 15:01:46 +0600

    Update stringMatching.cpp

--- stringMatching.cpp
+++ stringMatching.cpp
@@ -12,3 +12,21 @@ void doKmp(string s)
     }
   }
 }
+
+
+
+/*
+z[i] means maximum length of substring starting from i matches with prefix.
+*/
+vector<int>zFunction(string s)
+{
+  int n=s.size();vector<int>z(n);
+  z[0]=0;//l,r maintains a window with maximum r
+  for(int i=1,l=0,r=0;i<n;i++)
+  {
+    if(i<=r)z[i]=min(r-i+1,z[i-l]);//inside the window
+    while(i+z[i]<n&&s[z[i]]==s[i+z[i]])z[i]++;//brute-force
+    if(i+z[i]-1>r)l=i,r=i+z[i]-1;//r update
+  }
+  return z;
+}
